144ffd6a66b58f57f868d206a13d3ab47c1cfb5d
Mike Harder <mharder@microsoft.com>
2017-05-25 14:36:50 -0700

    Console app with a package reference

---
+++ NetCoreConsoleWithPackageReference/Program.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace NetCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine(typeof(IWebDriver));
+        }
+    }
+}
